45dca1d7496c194251490efd52deccd4cf328234
Jirez Tchinda <jireztchinda@gmail.com>
2017-08-10 00:11:13 +0100

    Ajout de getters et setters au bean Livre

--- Livre.java
+++ Livre.java
@@ -1,7 +1,26 @@
 import java.util.Date;
 
 public class Livre{
-  private String nom;
-  private Date dateParution;
-  private boolean disponible;
+	private String nom;
+	private Date dateParution;
+	private boolean disponible;
+
+  	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Date getDateParution() {
+		return dateParution;
+	}
+	public void setDateParution(Date dateParution) {
+		this.dateParution = dateParution;
+	}
+	public boolean isDisponible() {
+		return disponible;
+	}
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
 }
